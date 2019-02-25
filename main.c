7ff4590203068b7bc6e7dfe1d8755d1a83b3bea8
PrestonRobotics <47195211+PrestonRobotics@users.noreply.github.com>
2019-02-25 16:11:53 -0600

    Update main.c

--- main.c
+++ main.c
@@ -5,23 +5,28 @@ int main()
     printf("Hello World\n") ;
     create_connect() ;
     wait_for_light(2) ;
-    shut_down_in(119) ; 						// Waits for light in specified port # of milliseconds
+    shut_down_in(119) ; 				// Waits for light in specified port # of milliseconds
     
-       create_drive_direct(0,0) ;					// Drives the create at the left and right motor speeds (in mm per sec)
-    msleep(1500) ;								// Waits specified number of milliseconds before next line
+    create_drive_direct(0,0) ;				// Drives the create at the left and right motor speeds (in mm per sec)
+    msleep(1500) ;					// Waits specified number of milliseconds before next line
 	
-    create_drive_direct(200, 200) ;				// Moves forward at ... mm/sec
-    msleep(1500) ;
-    
-    create_drive_direct(200, -200) ;			// Create will turn right
+    create_drive_direct(200, -200) ;			
     msleep(1000) ;
     
-    create_drive_direct(200, 200) ;
+    create_drive_direct(200, 200) ;			
     msleep(1000) ;
     
-    Create_drive_direct(-200, 200)
-    msleep(1000)
-            create_drive_direct(-200, 200) ;
+	
+	
+	
+	
+    create_drive_direct(-200, 200) ;
+    msleep(1000) ;
+    
+    Create_drive_direct(-200, 200) ;
+    msleep(1000) ;
+
+    create_drive_direct(-200, 200) ;
     msleep(1500) ;
     
     create_drive_direct(200, 200) ;
@@ -33,8 +38,8 @@ int main()
     create_drive_direct(200, 200) ;
     msleep(1000)
     
-    create_stop() ;								// Turns off the Create motors
-    create_disconnect() ;						// Disconnects from the Create
+    create_stop() ;					// Turns off the Create motors
+    create_disconnect() ;				// Disconnects from the Create
     return 0;
     
 }
